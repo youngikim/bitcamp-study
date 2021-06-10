05392bb109b6b99e5e08b4b222a82d1c7cec9fe3
youngikim <youngikim427@gmail.com>
2021-06-10 20:53:45 +0900

    last of the day

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Exam0250.java
@@ -0,0 +1,10 @@
+package com.eomcs.lang.ex03;
+
+public class Exam0250 {
+  public static void main(String[] args) {
+    System.out.println(23);
+
+    System.out.println(-23);
+
+  }
+}
---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Exam0310.java
@@ -0,0 +1,16 @@
+package com.eomcs.lang.ex03;
+
+public class Exam0310 {
+    public static void main(String[] args) {
+
+        //10진수 표기법
+        System.out.println(3.14);
+
+        //Exponential 기호 사용하기
+        System.out.println(0.0314e2);
+        System.out.println(0.314e1);
+        System.out.println(31.4e-1);
+        System.out.println(314e-2);
+    }
+    
+}
