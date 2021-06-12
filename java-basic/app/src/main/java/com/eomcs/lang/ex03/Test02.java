23ca63676d0b39f52c08f6dce74c28823f9dee4c
youngikim <youngikim427@gmail.com>
2021-06-12 23:58:31 +0900

    saturday

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Test02.java
@@ -0,0 +1,15 @@
+package com.eomcs.lang.ex03;
+
+// 한 - 리터럴
+// en - literal
+class Test01 {
+    public static void main(String[] args) {
+        System.out.println("100");
+        System.out.println("3.14");
+        System.out.println("A");
+        System.out.println("가");
+        System.out.println(true);
+        System.out.println("ABC가각간");
+    
+    }
+}
